8c59bd90b1de1ee3b5f1fa304fd6304046720376
Roland Bouman <Roland.Bouman@gmail.com>
2026-04-24 11:24:43 +0200

    fixes

--- demo/samples.js
+++ demo/samples.js
@@ -1,6 +1,7 @@
 const hilitedSamples = {
   "duckdbsql": {
     label: "SQL (DuckDB)",
+    extensions: '.sql',
     highlighterPrefix: 'hilited-duckdb',
     styles: 'duckdbsql.css',
     regexp: /(?:(?<keyword>(?i-s:\b(?:a(?:t|ll|n(?:[dy]|aly(?:se|ze)|ti)|rray|s(?:c|of|ymmetric)?|uthorization)|b(?:y|etween|i(?:t|gint|nary)|o(?:olean|th))|c(?:as[et]|h(?:ar(?:acter)?|eck)|o(?:alesce|l(?:lat(?:e|ion)|umn(?:s)?)|n(?:currently|straint))|r(?:eate|oss))|d(?:o|e(?:c(?:imal)?|f(?:ault|errable)|sc(?:ribe)?)|istinct)|e(?:lse|nd|x(?:cept|ists|tract))|f(?:alse|etch|loat|or(?:eign)?|r(?:eeze|om)|ull)|g(?:enerated|lob|roup(?:ing(?:_id)?)?)|having|i(?:like|n(?:itially|ner|out|t(?:o|e(?:ger|r(?:sect|val)))?)?|s(?:null)?)|join|l(?:a(?:mbda|teral)|e(?:ading|ft)|i(?:ke|mit))|map|n(?:at(?:ional|ural)|char|o(?:ne|t(?:null)?)|u(?:ll(?:if)?|meric))|o(?:ffset|n(?:ly)?|r(?:der)?|ut(?:er)?|verla(?:y|ps))|p(?:ivot(?:_(?:longer|wider))?|lacing|osition(?:al)?|r(?:ecision|imary))|qualify|r(?:e(?:al|ferences|turning)|ight|ow)|s(?:e(?:lect|mi|tof)|how|imilar|mallint|ome|truct|u(?:bstring|mmarize)|ymmetric)|t(?:o|able(?:sample)?|hen|ime(?:stamp)?|r(?:ailing|eat|im|ue|y_cast))|u(?:n(?:i(?:on|que)|p(?:ack|ivot))|sing)|v(?:a(?:lues|r(?:char|iadic))|erbose)|w(?:he(?:n|re)|i(?:ndow|th))|xml(?:attributes|concat|e(?:lement|xists)|forest|namespaces|p(?:i|arse)|root|serialize|table))\b))|(?<number>(?:\d+(\.\d*)?|\.\d+))|(?<string>(?<!')'(?:''|''|[^'])*'(?!'))|(?<multiLineComment>\/\*(?:(?!\*\/)[\s\S])*\*\/)|(?<singleLineComment>--(?:(?!\n)[\s\S])*(?=\n))|(?<punctuation>[\(\)\{\}\.\:;,\-\+<>=\*])|(?<quotedIdentifier>(?<!")"(?:""|""|[^"])+"(?!"))|(?<identifier>\w+)|(?<whitespace>\s+))/gs,
@@ -12,6 +13,7 @@ const hilitedSamples = {
   },
   "json": {
     label: "JSON",
+    extensions: '.json',
     highlighterPrefix: 'hilited-json',
     styles: 'json.css',
     regexp: /(?:(?<punctuation>[{}\[\]:,])|(?<key>(?:"(?:\\"|\\\\|[^"\\])*")*(?=\s*:))|(?<string>"(?:\\"|\\\\|[^"\\])*")|(?<number>-?(?:0|[1-9]\d*)(?:\.\d+(?:[Ee][+-]?\d+)?|[Ee][+-]?\d+)?)|(?<keyword>\b(?:false|null|true)\b)|(?<whitespace>\s+))/gs ,
